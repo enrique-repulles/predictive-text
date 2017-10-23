e35911d90d2c54e307fa8c94b2f518d791a81357
Enric Repullés Moreno <46139958A@externos.santalucia.es>
2017-10-23 15:02:08 +0200

    Changes

--- get_data.R
+++ get_data.R
@@ -1,6 +1,13 @@
+
 dir.create("raw_data")
+dir.create("clean_data")
 setwd("raw_data")
-url="https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
-download.file(url=url, destfile = "Coursera-SwiftKey.zip")
-unzip("Coursera-SwiftKey.zip")
+
+#url="https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+#download.file(url=url, destfile = "Coursera-SwiftKey.zip")
+#unzip("Coursera-SwiftKey.zip")
+
+download.file(url="https://www.frontgatemedia.com/new/wp-content/uploads/2014/03/Terms-to-Block.csv", destfile = "Terms-to-Block.csv")
 setwd("..")
+
+
